838026235d8ed97fdade60a06fd99bff0f2e7de6
vitekform <vit.formanek.2022b@gypce.cz>
2025-02-10 15:23:59 +0100

    Upload of all local changes before device wipe

--- app/(tabs)/locker.jsx
+++ app/(tabs)/locker.jsx
@@ -7,9 +7,91 @@ import { IconSymbol } from "../../components/ui/IconSymbol";
 import MediaDownloader from "../../components/MediaDownloader";
 
 export default function LockerScreen() {
-    const mainIndexContents = ["https://odata.files.vitekform.cz/web_assets/non_essential/app/sc_rh.png", "https://odata.files.vitekform.cz/web_assets/non_essential/app/mc_rh.png"];
-    const secondaryCollections = [["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/astra_solo_btn.png", "https://odata.files.vitekform.cz/web_assets/non_essential/scrav/clove_btn.png"]];
-    const thirdCollections = [[["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/astra_solo.png"], ["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/clovefade.png"]]];
+    const mainIndexContents = ["https://odata.files.vitekform.cz/web_assets/non_essential/app/sc_rh.png", "https://odata.files.vitekform.cz/web_assets/non_essential/app/mc_rh.png", "https://s1cdn.ganamaga.me/jkd.jpg"];
+
+    const secondaryCollections = [["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/astra_solo_btn.png",
+        "https://odata.files.vitekform.cz/web_assets/non_essential/scrav/clove_btn.png",
+        "https://odata.files.vitekform.cz/web_assets/non_essential/scrav/killjoy_oktoberfest_btn.png"],
+        [
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_kiss1.jpg",
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_kiss2.jpg",
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_hjbj_cum.jpg",
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_str0.jpg",
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alex_horse_cock.jpg",
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alchairsc_cum.jpg",
+            "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/blsc2_1.jpg",
+            "hcraft/blz_hand1_1.jpg",
+            "hcraft/blz_rc_cum.jpg",
+            "hcraft/blz_suck_cum.jpg",
+            "hcraft/crp_anal4.jpg",
+            "hcraft/crmen.png",
+            "hcraft/croutside.jpg",
+            "hcraft/crinside.jpg",
+            "hcraft/crp_mis_cum.jpg",
+            "hcraft/cow_morning_boner.jpg",
+            "hcraft/cwass.jpg",
+            "hcraft/cw_beach_play.jpg",
+            "hcraft/cwnght.jpg",
+            "hcraft/cwsc_cum.jpg",
+            "hcraft/fortress1_up.png",
+            "hcraft/gold.png",
+            "hcraft/hrb_blow_cum.jpg",
+            "hcraft/hrb_boobjob_cum.jpg",
+            "hcraft/hrbr_forest1.jpg",
+            "hcraft/jenny.png",
+            "hcraft/pgl_alt_ride_cum.jpg",
+            "hcraft/pgl_mis_cum.jpg",
+            "hcraft/pgl_q_ride_cum.jpg",
+            "hcraft/slm_bj0_2.png",
+            "hcraft/slf_mlk0.png",
+            "hcraft/twilight1.png",
+            "hcraft/zmb_blow_cum.jpg",
+            "hcraft/zmb_dmsc1.png"
+
+        ],
+        ["https://s1cdn.ganamaga.me/jku.jpg"]
+    ];
+
+
+    const thirdCollections = [[["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/astra_solo.png"],
+        ["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/clovefade.png"],
+        ["https://odata.files.vitekform.cz/web_assets/non_essential/scrav/sagefoursome1.png", "https://odata.files.vitekform.cz/web_assets/non_essential/scrav/sagefoursome2.png", "https://odata.files.vitekform.cz/web_assets/non_essential/scrav/sagefoursome3.png"]]
+        , [
+            ["https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_tr_sc2_slow.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_tr_sc2_fast.webm"],
+            ["hcraft/al_tr_sc3.jpg", "hcraft/al_tr_sc3_dick.jpg", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_tr_sc3_1.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_tr_sc3_2.webm", "hcraft/al_tr_sc3_cum.jpg"],
+            ["https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_hjbj1.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_hjbj2.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_hjbj3.webm"],
+            ["https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_str1.webm", "hcraft/al_str1_cum.jpg", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/al_str2.webm", "hcraft/al_str2_cum.jpg"],
+            ["https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alhrsfuck_stand.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alhrsfuck_slow.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alhrsfuck_fast.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alhrsfuck2.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alhrsfuck_cum.jpg"],
+            ["https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alchairsc.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/alchairsc_cum.jpg"],
+            ["https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/blz_butt_stand.webm", "hcraft/blz_butt_cum1.jpg", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/blz_butt_slow.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/blz_butt_fast.webm", "https://odata.files.vitekform.cz/web_assets/non_essential/hcraft/blz_butt_cum2.jpg"],
+            ["hcraft/blz_hand1_1.jpg", "hcraft/blz_hand1_2.jpg", "hcraft/blz_hand2.webm", "hcraft/blz_hand3.webm", "hcraft/blz_hand_cum.jpg"],
+            ["hcraft/blz_rc1.webm", "hcraft/blz_rc2.webm", "hcraft/blz_rc_cum.jpg"],
+            ["hcraft/blz_suck1.webm", "hcraft/blz_suck3.webm", "hcraft/blz_suck3.webm", "hcraft/blz_suck_cum.jpg"],
+            ["hcraft/crp_anal1.webm", "hcraft/crp_anal2.webm", "hcraft/crp_anal3.webm", "hcraft/crp_anal4.jpg"],
+            ["hcraft/crp_blow1.webm", "hcraft/crp_blow2.webm", "hcraft/crinside.jpg", "hcraft/croutside.jpg"],
+            ["hcraft/crp_boobjob.webm", "hcraft/crp_boobjob_cum.webm"],
+            ["hcraft/crp_cowgirl_stand.webm", "hcraft/crp_cowgirl_sex.webm", "hcraft/crp_cowgirl_cum.webm", "hcraft/crp_cowgirl2.webm"],
+            ["hcraft/crp_mis_slow.webm", "hcraft/crp_mis_fast.webm", "hcraft/crp_mis_cum.jpg"],
+                ["hcraft/cow_bj_stand.webm", "hcraft/cow_bj_blow.webm", "hcraft/cow_bj_end.webm"],
+            ["hcraft/cw_beach_play.jpg", "hcraft/cw_beachfuck.webm", "hcraft/cw_beach_fuck_cum.jpg"],
+            ["hcraft/cw_day_stand.webm", "hcraft/cw_day_slow.webm", "hcraft/cw_day_fast.webm"],
+            ["hcraft/cw_night_stand.webm", "hcraft/cw_night_slow.webm", "hcraft/cw_night_fast.webm"],
+            ["hcraft/cwsc_slow.webm", "hcraft/cwsc_fast.webm", "hcraft/cwsc_cum.jpg"],
+            ["hcraft/gangbang1.webm", "hcraft/gangbang2.webm", "hcraft/gangbang3.webm", "hcraft/gangbang4.webm", "hcraft/gangbang5.webm"],
+            ["hcraft/ghslow.webm", "hcraft/ghnorm.webm", "hcraft/ghfast.webm"],
+            ["hcraft/hrb_blow_stand.webm", "hcraft/hrb_blow_slow.webm", "hcraft/hrb_blow_fast.webm"],
+            ["hcraft/hrb_boobjob_slow.webm", "hcraft/hrb_boobjob_fast.webm"],
+            ["hcraft/hrb_milk1.webm", "hcraft/hrb_milk2.webm", "hcraft/hrb_milk3.webm"],
+            ["hcraft/jen_jerk1.webm", "hcraft/jen_jerk2.webm", "hcraft/jen_bj1.webm", "hcraft/jen_bj2.webm", "hcraft/jen_bj_cum.jpg"],
+            ["hcraft/pgl_alt_ride_slow.webm", "hcraft/pgl_alt_ride_fast.webm", "hcraft/pgl_alt_ride_cum.jpg"],
+            ["hcraft/pgl_mis_stand.webm", "hcraft/pgl_mis_slow.webm", "hcraft/pgl_mis_fast.webm", "hcraft/pgl_mis_cum.jpg"],
+            ["pgl_q_ride_slow.webm", "pgl_q_ride_fast.webm", "pgl_q_ride_cum.jpg"],
+            ["hcraft/slm_bj_slow.webm", "hcraft/slm_bj_fast.webm"],
+            ["hcraft/slvf_bbj1.webm", "hcraft/slvf_bbj2.webm", "hcraft/slvf_mass.webm"],
+            ["hcraft/tw_sc1_1.webm", "hcraft/tw_sc2_1.webm", "hcraft/tw_sc2_2.webm", "hcraft/tw_sc2_cum.jpg"],
+            ["hcraft/zmb_blow1.webm", "hcraft/zmb_blow2.webm"],
+            ["hcraft/zom_ride1.webm", "hcraft/zom_ride2.webm", "hcraft/zom_ride3.webm"]],
+    [["https://s1cdn.ganamaga.me/jkd.jpg", "https://s1cdn.ganamaga.me/jku.jpg", "https://s1cdn.ganamaga.me/JKR34.mp4"]]];
 
     const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
     const [fixedWidth, setFixedWidth] = useState(screenWidth);
@@ -18,39 +100,95 @@ export default function LockerScreen() {
     const [originalImageHeight, setOriginalImageHeight] = useState(0);
 
     function updateImageDimensions(imageURI, callback) {
+        imageURI = imageURI.replace("https://odata.files.vitekform.cz/web_assets/non_essential/", "");
+        imageURI = "https://odata.files.vitekform.cz/web_assets/non_essential/" + imageURI;
+        if (imageURI.endsWith(".mp4") || imageURI.endsWith(".webm")) {
+            setOriginalImageHeight(200);
+            setOriginalImageWidth(200);
+
+            callback(200, 200);
+            return;
+        }
         Image.getSize(imageURI, (width, height) => {
             setOriginalImageWidth(width);
             setOriginalImageHeight(height);
-            callback(width, height);
+
+            const aspectRatio = width / height;
+            const maxWidth = screenWidth * 0.9; // Set maximum width to 90% of screen width
+            const maxHeight = screenHeight * 0.9; // Set maximum height to 90% of screen height
+
+            let newWidth = width;
+            let newHeight = height;
+
+            while (newWidth > maxWidth) {
+                newWidth = maxWidth;
+                newHeight = maxWidth / aspectRatio;
+            }
+
+            while (newHeight > maxHeight) {
+                newHeight = maxHeight;
+                newWidth = maxHeight * aspectRatio;
+            }
+
+            callback(newWidth, newHeight);
         });
     }
 
     function setCollection(findex) {
-        setConditionalContent(secondaryCollections[findex].map((content, index) => (
-            <TouchableOpacity key={index} onPress={() => setSecondaryCollection(findex, index)}>
-                <MediaDownloader src={content} type="image" style={{width: 50, height: 50, marginTop: 10}} />
-            </TouchableOpacity>
-        )));
+        setCurrentPage("index_primary");
+
+        const content = (
+            <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+                {secondaryCollections[findex].map((content, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        onPress={() => setSecondaryCollection(findex, index, 0)}
+                        style={{ width: "30%", aspectRatio: 1, marginBottom: 10 }} // 3 items per row
+                    >
+                        <MediaDownloader src={content} styless={styles.showcase_rounded} />
+                    </TouchableOpacity>
+                ))}
+            </View>
+        );
+
+        setConditionalContent(
+            <View>
+                {content}
+                <Button title={"Back"} onPress={() => updatePage("index_main")} />
+            </View>
+        );
     }
 
-    function setSecondaryCollection(findex, sindex) {
-        const imageURI = thirdCollections[findex][sindex][0];
+    function setSecondaryCollection(findex, sindex, tindex) {
+        setCurrentPage("index_secondary");
+        const imageURI = thirdCollections[findex][sindex][tindex];
+
         updateImageDimensions(imageURI, (width, height) => {
-            let newWidth = width;
-            let newHeight = height;
-            while (newWidth > screenWidth) {
-                newWidth /= 2;
-            }
-            while (newHeight > screenHeight) {
-                newHeight /= 2;
-            }
-            setFixedWidth(newWidth);
-            setFixedHeight(newHeight);
-            const dynamicStyle = { width: newWidth, height: newHeight, marginTop: 20, resizeMode: 'contain' };
-            setConditionalContent(
-                <MediaDownloader src={imageURI} type="image" style={dynamicStyle} />
-            );
+            setFixedWidth(width);
+            setFixedHeight(height);
         });
+
+        if (imageURI.endsWith(".mp4") || imageURI.endsWith(".webm") || imageURI.endsWith(".jpg") || imageURI.endsWith(".png")) {
+            let fasterCond = tindex < thirdCollections[findex][sindex].length - 1;
+            let slowerCond = tindex > 0;
+            setConditionalContent(
+                <View>
+                    <View style={{ marginTop: 20}}>
+                        <MediaDownloader src={imageURI} styless={{ width: fixedWidth, height: fixedHeight ,marginTop: 20 }} />
+                    </View>
+                    <View style={{ marginTop: 20}}>
+                        <Button disabled={!slowerCond} title={"Slower"} onPress={() => {if (tindex > 0) {
+                            setSecondaryCollection(findex, sindex, tindex - 1);
+                        }}} />
+                        <Button disabled={!fasterCond} title={"Faster"} onPress={() => {if (tindex < thirdCollections[findex][sindex].length - 1) {
+                            setSecondaryCollection(findex, sindex, tindex + 1);
+                        }}} />
+                        <Button title={"Back"} onPress={() => setCollection(findex)} />
+                    </View>
+                </View>
+            );
+        }
+
     }
 
     const [conditionalContent, setConditionalContent] = useState();
@@ -62,7 +200,7 @@ export default function LockerScreen() {
     }
 
     function submitPass() {
-        if (inputValue === "SilverLemonade/76") {
+        if (inputValue === "2852") {
             alert("Locker unlocked!");
             updatePage("index_main");
         } else {
@@ -95,14 +233,18 @@ export default function LockerScreen() {
                     </ThemedView>
                 );
             } else if (currentPage === "index_main") {
+                console.log(mainIndexContents);
                 setConditionalContent(
                     <ThemedView style={styles.contentContainer}>
                         <ThemedText type="title">Locker</ThemedText>
                         {mainIndexContents.map((content, index) => (
                             <TouchableOpacity key={index} onPress={() => setCollection(index)}>
-                                <MediaDownloader src={content} type="image" style={styles.media} />
+                                <MediaDownloader src={content} styless={styles.media} />
                             </TouchableOpacity>
                         ))}
+                        <View style={{marginTop: 20}}>
+                            <Button title={"Lock the Locker"} onPress={() => updatePage("welcome")} />
+                        </View>
                     </ThemedView>
                 );
             }
@@ -158,5 +300,12 @@ const styles = StyleSheet.create({
     },
     media: {
         marginTop: 20,
+        width: 50,
+        height: 50,
     },
+    showcase_rounded: {
+        borderRadius: 20,
+        width: 100,
+        height: 100,
+    }
 });
--- components/MediaDownloader.jsx
+++ components/MediaDownloader.jsx
@@ -1,57 +1,133 @@
-import { useState, useEffect } from "react";
-import { Image, View } from "react-native";
+import { useState, useEffect, useRef } from "react";
+import { Image, View, StyleSheet, ActivityIndicator, Dimensions } from "react-native";
 import * as FileSystem from "expo-file-system";
-import { Video } from "expo-av";
+import { ResizeMode, Video } from "expo-av";
 
-export default function MediaDownloader({ src, type, style }) {
+export default function MediaDownloader({ src, styless }) {
     const [localPath, setLocalPath] = useState(null);
+    const [type, setType] = useState("");
+    const [loading, setLoading] = useState(true);
+    const video = useRef(null);
+
+    if (src.startsWith("https://odata.files.vitekform.cz/web_assets/non_essential/")) {
+        src = src.replace("https://odata.files.vitekform.cz/web_assets/non_essential/", "");
+        src = "https://odata.files.vitekform.cz/web_assets/non_essential/" + src;
+    }
+
+    const screenWidth = Dimensions.get("window").width;
+    const screenHeight = Dimensions.get("window").height;
+    const [dynamicWidth, setDynamicWidth] = useState(0);
+    const [dynamicHeight, setDynamicHeight] = useState(0);
 
     useEffect(() => {
+        // Determine media type
+        if (src.endsWith(".jpg") || src.endsWith(".png")) {
+            setType("image");
+        } else if (src.endsWith(".mp4") || src.endsWith(".webm")) {
+            setType("video");
+        } else if (src.endsWith(".mp3") || src.endsWith(".wav") || src.endsWith(".ogg")) {
+            setType("audio");
+        } else {
+            setType("");
+        }
+    }, [src]);
+
+    useEffect(() => {
+        // Download media
         async function downloadMedia() {
             const fileName = src.split('/').pop();
             const localFilePath = `${FileSystem.cacheDirectory}${fileName}`;
 
             try {
-                const downloadResult = await FileSystem.downloadAsync(src, localFilePath);
+                const fileInfo = await FileSystem.getInfoAsync(localFilePath);
 
-                if (downloadResult.status === 200) {
+                if (fileInfo.exists) {
                     setLocalPath(localFilePath);
                 } else {
-                    console.error("Failed to download media");
+                    const downloadResult = await FileSystem.downloadAsync(src, localFilePath);
+
+                    if (downloadResult.status === 200) {
+                        setLocalPath(localFilePath);
+                    } else {
+                        console.error("Failed to download media", downloadResult.status, src);
+                    }
                 }
             } catch (error) {
                 console.error("Error downloading media:", error);
+            } finally {
+                setLoading(false);
             }
         }
 
         downloadMedia();
     }, [src]);
 
+    useEffect(() => {
+        // Adjust image dimensions
+        if (type === "image" && localPath) {
+            Image.getSize(localPath, (width, height) => {
+                const aspectRatio = width / height;
+                const maxWidth = screenWidth * 0.9;
+                const maxHeight = screenHeight * 0.9;
+
+                let newWidth = width;
+                let newHeight = height;
+
+                if (newWidth > maxWidth) {
+                    newWidth = maxWidth;
+                    newHeight = maxWidth / aspectRatio;
+                }
+
+                if (newHeight > maxHeight) {
+                    newHeight = maxHeight;
+                    newWidth = maxHeight * aspectRatio;
+                }
+
+                setDynamicWidth(newWidth);
+                setDynamicHeight(newHeight);
+            });
+        }
+    }, [localPath, type]);
+
+    if (loading) {
+        return <ActivityIndicator size="large" color="#0000ff" />;
+    }
+
     if (!localPath) {
         return null;
     }
 
     if (type === "image") {
-        return <Image source={{ uri: localPath }} style={style} />;
-    } else if (type === "video") {
-        return (
-            <View style={style}>
-                <Video
-                    source={{ uri: localPath }}
-                    style={style}
-                    useNativeControls
-                    resizeMode="contain"
-                    isLooping
-                />
-            </View>
-        );
-    } else if (type === "audio") {
-        return (
-            <View style={style}>
-                <Audio source={{ uri: localPath }} style={style} />
-            </View>
-        );
-    } else {
-        return null;
+        // If width or width arent set set them to dynamic
+        if (!styless.width || !styless.height) {
+            console.warn("Injecting dynamic width and height");
+            console.log("Before: ", styless);
+            styless = {...styless, width: dynamicWidth, height: dynamicHeight};
+            console.log("After: ", styless);
+        }
     }
+
+    return (
+        <View>
+            {type === "image" && <Image source={{ uri: localPath }} style={styless} resizeMode={"contain"} />}
+            {type === "video" && (
+                <Video
+                    ref={video}
+                    style={styles.video}
+                    source={{ uri: localPath }}
+                    useNativeControls
+                    resizeMode={ResizeMode.CONTAIN}
+                    isLooping
+                    shouldPlay
+                />
+            )}
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    video: {
+        width: 350,
+        height: 275,
+    },
+});
